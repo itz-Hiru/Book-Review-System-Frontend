ebbc034377bd8972dd3671349fec84358419f810
itz-Hiru <hirumithagithub@gmail.com>
2025-03-29 15:59:35 +0530

    Completed project

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 export default [
   { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx,css}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "animate.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("Root element not found. Make sure you have <div id='root'></div> in your index.html.");
+}
+
+createRoot(root).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
